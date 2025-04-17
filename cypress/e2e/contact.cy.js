153890a1b7a9963889b17e4cef4c0ada4fbbbd7d
Nolwenn <nolwenn.pennec@hotmail.com>
2025-04-17 15:05:51 +0200

    amélioration contact.cy + amélioration qualité photo fond

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -1,62 +1,53 @@
 import { faker } from '@faker-js/faker';
 
-describe('Contact Form - Soumission valide', () => {
-    it('remplit et envoie le formulaire avec succès', () => {
-        cy.visit('');
-
+describe('Formulaire de contact', () => {
         const fauxNom = faker.person.fullName();
         const fauxEmail = faker.internet.email();
         const fauxMessage = faker.lorem.paragraph();
 
-        cy.getBySel('contact-name').type(fauxNom);
-        cy.getBySel('contact-email').type(fauxEmail);
-        cy.getBySel('contact-message').type(fauxMessage);
-        cy.getBySel('contact-send').click();
-
-        cy.getBySel('success-message').should('be.visible').and('contain', 'Message envoyé avec succès');
-    });
-});
-
-describe('Contact Form - Validation des champs', () => {
     beforeEach(() => {
         cy.visit('');
     });
 
-    it('affiche un message d’erreur si tous les champs sont vides', () => {
-        cy.getBySel('contact-send').click();
-        cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+    describe('Soumission valide', () => {
+        it('remplit et envoie le formulaire avec succès', () => {
+            cy.getBySel('contact-name').type(fauxNom);
+            cy.getBySel('contact-email').type(fauxEmail);
+            cy.getBySel('contact-message').type(fauxMessage);
+            cy.getBySel('contact-send').click();
+
+            cy.getBySel('success-message').should('be.visible').and('contain', 'Message envoyé avec succès');
+        });
     });
 
-    it('affiche une erreur si le nom est vide', () => {
-        const fauxEmail = faker.internet.email();
-        const fauxMessage = faker.lorem.paragraph();
+    describe('Validation des champs obligatoires', () => {
+        it('affiche une erreur si tous les champs sont vides', () => {
+            cy.getBySel('contact-send').click();
+            cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+        });
 
-        cy.getBySel('contact-email').type(fauxEmail);
-        cy.getBySel('contact-message').type(fauxMessage);
-        cy.getBySel('contact-send').click();
+        it('affiche une erreur si le nom est vide', () => {
+            cy.getBySel('contact-email').type(fauxEmail);
+            cy.getBySel('contact-message').type(fauxMessage);
+            cy.getBySel('contact-send').click();
 
-        cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
-    });
+            cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+        });
 
-    it('affiche une erreur si l’email est vide', () => {
-        const fauxNom = faker.person.fullName();
-        const fauxMessage = faker.lorem.paragraph();
+        it('affiche une erreur si l’email est vide', () => {
+            cy.getBySel('contact-name').type(fauxNom);
+            cy.getBySel('contact-message').type(fauxMessage);
+            cy.getBySel('contact-send').click();
 
-        cy.getBySel('contact-name').type(fauxNom);
-        cy.getBySel('contact-message').type(fauxMessage);
-        cy.getBySel('contact-send').click();
+            cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+        });
 
-        cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
-    });
+        it('affiche une erreur si le message est vide', () => {
+            cy.getBySel('contact-name').type(fauxNom);
+            cy.getBySel('contact-email').type(fauxEmail);
+            cy.getBySel('contact-send').click();
 
-    it('affiche une erreur si le message est vide', () => {
-        const fauxNom = faker.person.fullName();
-        const fauxEmail = faker.internet.email();
-
-        cy.getBySel('contact-name').type(fauxNom);
-        cy.getBySel('contact-email').type(fauxEmail);
-        cy.getBySel('contact-send').click();
-
-        cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+            cy.getBySel('response-message').should('contain', 'Tous les champs sont requis');
+        });
     });
 });
